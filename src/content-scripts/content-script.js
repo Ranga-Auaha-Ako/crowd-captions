b1cdd8eeba9d889ffbb6b9f28c89362a17270024
Cory <coryedwinsmith@gmail.com>
2021-08-30 17:28:54 +1200

    initial commit
    Co-authored-by: Zac M-W <HexaCubist@users.noreply.github.com>

--- src/content-scripts/content-script.js
+++ src/content-scripts/content-script.js
@@ -5,7 +5,9 @@ console.log('Hello from the content-script');
 
 const appContainer = document.createElement('div');
 appContainer.id = 'crowdcaptions-app-interface';
-document.getElementById('eventTabControl').appendChild(appContainer);
+document.getElementById('dockedCaption').replaceChildren(appContainer);
+// .appendChild(appContainer);
+// document.getElementById('dockedCaption').textContent = 'testing';
 
 /* eslint-disable no-new */
 new Vue({
