1bb65944b6f1cbd16fe19c07a711b54644a6edd9
Zac Miller-Waugh <z.millerwaugh@auckland.ac.nz>
2021-08-31 17:27:46 +1200

    Implement VueX

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,36 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    captionTexts: [
+      {
+        start: 0,
+        edits: [
+          { text: 'Hello, this is alt 1!', votes: 0, userVote: 0 },
+          { text: 'Hello, this is alt 2!', votes: 0, userVote: 0 },
+          { text: 'Hello, this is alt 3!', votes: 0, userVote: 0 },
+          { text: 'Hello, this is alt 4!', votes: 0, userVote: 0 },
+        ],
+      },
+      {
+        start: 10,
+        edits: [
+          { text: 'Another one: 1', votes: 0, userVote: 0 },
+          { text: 'Another one: 2', votes: 0, userVote: 0 },
+          { text: 'Another one: 3', votes: 0, userVote: 0 },
+          { text: 'Another one: 4', votes: 0, userVote: 0 },
+        ],
+      },
+    ],
+  },
+  getters: {
+    currentCaption(state) {
+      return state.captionTexts[0];
+    },
   },
   mutations: {
+    likeCaption(state, alternative) {
+      state.currentCaption[alternative] += 1;
+    },
   },
   actions: {
   },
