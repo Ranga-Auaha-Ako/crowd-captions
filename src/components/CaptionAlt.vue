bbc57f508c07af25feff39dc8fbf338a95e1704a
Cory <coryedwinsmith@gmail.com>
2021-09-11 15:04:45 +1200

    Updated logic, changes v-if not updating

--- src/components/CaptionAlt.vue
+++ src/components/CaptionAlt.vue
@@ -1,46 +1,48 @@
 <template>
-  <v-row>
-    <v-col>
-      <v-row>
-        <v-btn>
-          <v-icon left class="material-icons"> arrow_upward </v-icon>
-        </v-btn>
-      </v-row>
-      <v-row>
-        <v-btn>
-          <v-icon left class="material-icons"> arrow_downward </v-icon>
-        </v-btn>
-      </v-row>
-    </v-col>
-    <v-col>
-      <!-- displays the current - 
-      since App passes in the content at the given index anyway, 
-      index is not used here -->
-      <!-- <div :content="edit"></div> -->
-      <p>{{ edit.text }}</p>
-      <v-text-field
-        :hidden="setDisabled(true)"
-        v-model="edited"
-        :label="Regular"
-        @keydown.enter="editButton()"
-        clearable
-      >
-      </v-text-field>
-    </v-col>
-    <v-col>
-      <v-row>
-        <v-btn @click="setDisabled(false)">
-          <v-icon right class="material-icons"> create </v-icon>
-        </v-btn>
-      </v-row>
-      <v-row>
-        <v-btn v-if="index == 0">
-          <!-- index used here; if first Cap - display the dropdown -->
-          <v-icon right class="material-icons"> view_list </v-icon>
-        </v-btn>
-      </v-row>
-    </v-col>
-  </v-row>
+  <div v-if="index == 0 || showEdits">
+    <v-row>
+      <v-col>
+        <v-row>
+          <v-btn>
+            <v-icon left class="material-icons"> arrow_upward </v-icon>
+          </v-btn>
+        </v-row>
+        <v-row>
+          <v-btn>
+            <v-icon left class="material-icons"> arrow_downward </v-icon>
+          </v-btn>
+        </v-row>
+      </v-col>
+      <v-col>
+        <!-- displays the current - 
+        since App passes in the content at the given index anyway, 
+        index is not used here -->
+        <!-- <div :content="edit"></div> -->
+        <p>{{ edit.text }}</p>
+        <v-text-field
+          :hidden="setDisabled(true)"
+          v-model="edited"
+          :label="Regular"
+          @keydown.enter="editButton()"
+          clearable
+        >
+        </v-text-field>
+      </v-col>
+      <v-col>
+        <v-row>
+          <v-btn @click="setDisabled(false)">
+            <v-icon right class="material-icons"> create </v-icon>
+          </v-btn>
+        </v-row>
+        <v-row>
+          <v-btn @click="toggleShowEdits()" v-if="index == 0">
+            <!-- index used here; if first Cap - display the dropdown -->
+            <v-icon right class="material-icons"> view_list </v-icon>
+          </v-btn>
+        </v-row>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
@@ -50,11 +52,12 @@
 export default {
   props: {
     index: Number,
-    edit: String,
+    edit: Object,
   },
   data() {
     return {
       edited: "",
+      showEdits: false
     };
   },
   name: "CaptionAlt",
@@ -69,6 +72,10 @@ export default {
       // may need to add mutation this.$store.commit(mutation_meth, variable)
       this.setDisabled(true);
     },
+    toggleShowEdits(){
+      this.showEdits = !this.showEdits;
+      console.log(this.showEdits);
+    }
   },
 };
 </script>
