fbb42b341e4fa0cd0486a736e8e055aaa0fd280c
Zac Miller-Waugh <z.millerwaugh@auckland.ac.nz>
2021-12-09 11:32:21 +1300

    Add Vuetify to popup

--- src/popup/main.js
+++ src/popup/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue';
+import vuetify from "@/plugins/vuetify";
 import App from './App.vue';
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  vuetify,
   render: (h) => h(App),
 });
